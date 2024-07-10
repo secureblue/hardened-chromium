25aa79b384c3579217efd8a427ede6ab07f36a9a
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-07-10 12:53:51 -0700

    more fixes

--- copr_script.sh
+++ copr_script.sh
@@ -7,13 +7,10 @@ cd chromium-copr-experimental
 # Download chromium-%{version}-clean.tar.xz from the Fedora's server
 rpkg --path ./chromium sources
 
-cp patches.py ./chromium
+cp patches/* ./chromium
 
 # Patch the spec file to build with the vanadium patches
 patch -d ./chromium -p1 < vanadium.patch
-
-# Rename files that are called chromium-browser to vanadium to avoid name clashes
-# patch -d ./chromium -p1 < rename.patch
 
 # Move all the source files into the parent directory for the COPR build system to find them
 mv ./chromium/* ../
